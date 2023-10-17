71f1f67382c248b7b64ed06a6319f18ce6ec923f
BestStepInUa <beststep.in.ua@gmail.com>
2023-10-17 20:44:28 +0300

    live_page_working_fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 import {fetchBreeds, fetchCatByBreed} from './js/cat-api.js'
-import axios from 'axios';
-axios.defaults.headers.common["x-api-key"] = "live_PT1y6jyiP6j6Uou4H075ma9te5HwnwjNAaf8UWMRCahkfYJogPikmAOX28RvdITp";
 
 const select = document.querySelector('.breed-select');
 // select.addEventListener('select', onSelect);
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,11 +1,9 @@
-import axios from 'axios';
-axios.defaults.headers.common["x-api-key"] = "live_PT1y6jyiP6j6Uou4H075ma9te5HwnwjNAaf8UWMRCahkfYJogPikmAOX28RvdITp";
-axios.defaults.baseURL = 'https://api.thecatapi.com/v1';
 const BASE_API_URL = 'https://api.thecatapi.com/v1';
+const API_KEY = 'live_PT1y6jyiP6j6Uou4H075ma9te5HwnwjNAaf8UWMRCahkfYJogPikmAOX28RvdITp';
 
 export function fetchBreeds() {
     return fetch(
-        `${BASE_API_URL}/breeds`
+        `${BASE_API_URL}/breeds?x-api-key=${API_KEY}`
     ).then((response) => {
         if (!response.ok) {
             throw new Error(response.statusText);
@@ -16,7 +14,7 @@ export function fetchBreeds() {
 
 export function fetchCatByBreed(breedId) {
     return fetch(
-        `${BASE_API_URL}/images/search?breed_ids=${breedId}`
+        `${BASE_API_URL}/images/search?breed_ids=${breedId}&x-api-key=${API_KEY}`
     ).then((response) => {
         if (!response.ok) {
             throw new Error(response.statusText);
