acb5459d383a26ce073a8cb0fe92673780ff68fd
BestStepInUa <beststep.in.ua@gmail.com>
2023-10-18 16:49:41 +0300

    need_mentor_help

--- src/index.js
+++ src/index.js
@@ -1,14 +1,72 @@
-import {fetchBreeds, fetchCatByBreed} from './js/cat-api.js'
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api.js'
+import { Notify } from 'notiflix/build/notiflix-notify-aio.js';
+
+Notify.init({
+    width: '300px',
+    position: 'center-top',
+    fontSize: '16px',    
+});
 
 const select = document.querySelector('.breed-select');
-// select.addEventListener('select', onSelect);
+const loader = document.querySelector('.loader');
+// const error = document.querySelector('.error')
+const catInfoCard = document.querySelector('.cat-info');
+
+select.style.display = 'none';
+// error.style.display = 'none';
+catInfoCard.style.display = 'none';
+
+select.addEventListener('change', onSelect);
 
 fetchBreeds()
-    .then((breeds) => select.innerHTML = createSectionOptionsMarkup(breeds))
-    .catch((err) => console.error(err));
+    .then(breeds => {
+        select.style.display = "initial";
+        select.innerHTML = createSectionOptionsMarkup(breeds);
+    })
+    .catch((err) => {
+        console.error(err);
+        loader.style.display = 'none';
+        Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    })
+    .finally( _ => loader.style.display = 'none')
 
 function createSectionOptionsMarkup(breedsArr) {
     return breedsArr.map(({ id, name }) =>
         `<option value="${id}">${name}</option>`
     ).join('');
 };
+
+function onSelect(evt) {
+    loader.style.display = 'initial';
+
+    const breedId = evt.target.value;
+
+    fetchCatByBreed(breedId)
+        .then(catData => {
+            console.log(catData);
+            catInfoCard.style.display = 'initial';
+            createCatCardMarkup(catData);   
+        })
+        .catch((err) => {
+        console.error(err);        
+        Notify.failure('Oops! Something went wrong! Try reloading the page!');
+        })
+        .finally( _ => loader.style.display = 'none')
+}
+
+function createCatCardMarkup(catData) {
+    const {
+        breeds: { name, description, temperament },
+        url,
+    } = catData;
+
+    const catCard = `
+      <img class="cat-img" src="${url}" alt="${name}"  >
+      <div class="cat-text">
+      <h1 class="cat-name">${name}</h1>
+      <p class="cat-description">${description}</p>
+      <p class="cat-temperament"><span class="cat-temperament-span">Temperament:</span> ${temperament}</p>    
+      </div>`;
+    
+    catInfoCard.innerHTML = catCard;
+}
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -3,10 +3,10 @@ const API_KEY = 'live_PT1y6jyiP6j6Uou4H075ma9te5HwnwjNAaf8UWMRCahkfYJogPikmAOX28
 
 export function fetchBreeds() {
     return fetch(
-        `${BASE_API_URL}/breeds?x-api-key=${API_KEY}`
+        `${BASE_API_URL}/breeds?api_key=${API_KEY}`
     ).then((response) => {
         if (!response.ok) {
-            throw new Error(response.statusText);
+            throw new Error(response.statusText || response.status);
         }
         return response.json();
     })
@@ -14,10 +14,10 @@ export function fetchBreeds() {
 
 export function fetchCatByBreed(breedId) {
     return fetch(
-        `${BASE_API_URL}/images/search?breed_ids=${breedId}&x-api-key=${API_KEY}`
+        `${BASE_API_URL}/images/search?breed_ids=${breedId}&api_key=${API_KEY}`
     ).then((response) => {
         if (!response.ok) {
-            throw new Error(response.statusText);
+            throw new Error(response.statusText || response.status);
         }
         return response.json();
     })
